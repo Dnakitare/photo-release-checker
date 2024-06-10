1f6998eea8e1a0e5b765e7ac1ef82f85fe314ae2
Daniel Nakitare <dnakitare@gmail.com>
2024-06-09 22:03:12 -0600

    chore: Add configuration for upload and known_faces folders

--- app/models.py
+++ app/models.py
@@ -6,7 +6,3 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
